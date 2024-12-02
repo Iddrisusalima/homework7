b652bd1354ffb9c6d7a2cfff95a3c648b224c64d
Iddrisusalima <iddrisusalima733@gmail.com>
2024-12-02 13:17:59 +0000

    adding index.html

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// function processRegistration() {
+//   const name = document.getElementById('name').value;
+//   const age = parseInt(document.getElementById('age').value);
+//   const email = document.getElementById('email').value;
+
+//   if (age < 12) {
+//       alert(`Hi ${name}, you are ${age} years old and you are too young to register. Sorry 😔😔`);
+//   } else if (age >= 12 && age < 18) {
+//       alert(`Hi ${name}, you are ${age} years old and you have limited options to register for. We will keep in touch via your email: ${email} 👋🏿👋🏿`);
+//   } else {
+//       alert(`Hi ${name}, you are ${age} years old and you can register for any option of your choosing. We will keep in touch via your email: ${email} 👋🏿👋🏿`);
+//   }
+// }
+// let name=prompt("what is your name");
+// alert(`hello ${name}`);
+function food(){
+    let name=prompt("Tell us your name");
+     let age=prompt("what is your age");
+     let email=prompt("what is your email address");
+
+if (age>5) 
+alert(`hello ${name},you are ${age} years old and can access our products`); 
+else if( age<5)
+alert(`hello ${name},you are ${age} years old and not qualified`);
+  } ;
