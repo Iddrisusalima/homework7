2db263b8353b08318e33f0b20ed2e463e1f19650
Iddrisusalima <iddrisusalima733@gmail.com>
2024-12-13 15:28:45 +0000

    Add files via upload

---
+++ src/Script.js
@@ -0,0 +1,28 @@
+// function processRegistration() {
+//   const name = document.getElementById('name').value;
+//   const age = parseInt(document.getElementById('age').value);
+//   const email = document.getElementById('email').value;
+
+//   if (age < 12) {
+//       alert(`Hi ${name}, you are ${age} years old and you are too young to register. Sorry 😔😔`);
+//   } else if (age >= 12 && age < 18) {
+//       alert(`Hi ${name}, you are ${age} years old and you have limited options to register for. We will keep in touch via your email: ${email} 👋🏿👋🏿`);
+//   } else {
+//       alert(`Hi ${name}, you are ${age} years old and you can register for any option of your choosing. We will keep in touch via your email: ${email} 👋🏿👋🏿`);
+//   }
+// }
+// let name=prompt("what is your name");
+// alert(`hello ${name}`);
+function food() {
+  let name = prompt("Tell us your name");
+  let age = prompt("What is your age");
+  let email = prompt("What is your email address");
+
+  if (age > 5) {
+      alert(`Hello ${name}, you are ${age} years old and can access our products`);
+  } else if (age < 5) {
+      alert(`Hello ${name}, you are ${age} years old and not qualified`);
+  }
+}
+
+food();
